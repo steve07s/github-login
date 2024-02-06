13294dbbed4c6c62a2e39b314bc299ec6bcdc4b6
alesstongwen <alesstongwen@gmail.com>
2024-02-06 10:06:28 -0800

    add starter code

---
+++ middleware/checkAuth.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
+/*
+FIX ME (types) 😭
+*/
+export const ensureAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/auth/login");
+};
+
+/*
+FIX ME (types) 😭
+*/
+export const forwardAuthenticated = (req: any, res: any, next: any) => {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/dashboard");
+};
